c4f790e159a9a0201a9c3d299e95d83a5eb08aa3
Xander <xander.bosch@ahold.com>
2018-03-30 11:09:53 +0200

    update to gatsby-remark-embed-appietoday

--- build/index.js
+++ build/index.js
@@ -1,57 +1,57 @@
-'use strict'
+'use strict';
+
 const visit = require(`unist-util-visit`);
 const getVideoId = require('get-video-id');
 
 module.exports = ({ markdownAST }, options = { width: 560, ratio: 1.7, related: true }) => {
-    const createIframe = (url, videoPlatform) => {
-        if (options.ratio === undefined) {
-            options.ratio = 1.77;
-        }
+  const createIframe = (url, videoPlatform) => {
+    if (options.ratio === undefined) {
+      options.ratio = 1.77;
+    }
 
-        let height = options.height;
+    let height = options.height;
 
-        if (height === undefined) {
-            height = Math.round(options.width / options.ratio);
-        }
+    if (height === undefined) {
+      height = Math.round(options.width / options.ratio);
+    }
 
-        if (videoPlatform == 'youtube' && !(options.related)) {
-            url += '?rel=0';
-        }
+    if (videoPlatform == 'youtube' && !options.related) {
+      url += '?rel=0';
+    }
 
-        return `<iframe 
+    return `<iframe 
               width="${options.width}" 
               height="${height}" 
               src="${url}" 
               frameborder="0" 
               allowfullscreen
-            ></iframe>`
-    };
+            ></iframe>`;
+  };
 
-    const videoTypes = {
-        'appietoday': (id) => createIframe(`https://www.appietoday.nl/embed/bekijk/${id}`, 'youtube'),
-    }
+  const videoTypes = {
+    'appietoday': id => createIframe(`https://www.appietoday.nl/embed/bekijk/${id}`, 'youtube')
+  };
 
-    visit(markdownAST, `inlineCode`, node => {
-        const { value } = node;
+  visit(markdownAST, `inlineCode`, node => {
+    const { value } = node;
 
-        const processValue = value.match(/([^:]*):(.*)/);
-        if (processValue) {
-            let type = processValue[1];
-            let id = processValue[2];
+    const processValue = value.match(/([^:]*):(.*)/);
+    if (processValue) {
+      let type = processValue[1];
+      let id = processValue[2];
 
-            if (Object.keys(videoTypes).includes(type) && id) {
-                id = id.trim();
-                let videoId = getVideoId(id);
-                if (videoId) {
-                    type = videoId.service;
-                    id = videoId.id;
-                }
-                node.type = `html`
-                node.value = videoTypes[type](id);
-            }
+      if (Object.keys(videoTypes).includes(type) && id) {
+        id = id.trim();
+        let videoId = getVideoId(id);
+        if (videoId) {
+          type = videoId.service;
+          id = videoId.id;
         }
+        node.type = `html`;
+        node.value = videoTypes[type](id);
+      }
+    }
+  });
 
-    })
-
-    return markdownAST
-}
+  return markdownAST;
+};
